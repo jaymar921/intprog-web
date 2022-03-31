bae9e242e9d6611aa534d78fc8ef5e1ab6441c80
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-03-31 09:54:38 +0800

    basic web

--- ProjectFiles/web_app.py
+++ ProjectFiles/web_app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for,render_template
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return render_template("welcome.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", debug=True, port=5000)
