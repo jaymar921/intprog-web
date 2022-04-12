770c78e36397830501d892c2feae7088a310ff8a
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-04-12 08:54:37 +0800

    updated script.js

--- ProjectFiles/static/script.js
+++ ProjectFiles/static/script.js
@@ -1,17 +1,6 @@
 function confirm_password(password, confirm_password){
-    if(password== confirm_password){
-        postFunction();
-        location.href="/login";
-    }else{
+    if(password!= confirm_password){
         window.alert("password doesn't match!");
         document.getElementById("flag").value = "deny";
     }
-}
-
-function postFunction(){
-    var ajax = new XMLHttpRequest();
-    var data = document.getElementById("registration_form");
-    var formData = new FormData(data);
-    ajax.open("POST","/register_account", true);
-    ajax.send(formData);
 }
