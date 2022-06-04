98dffbd8e524e393ca0cac7a03243810127ce982
ha-rold1999 <htarimanc@gmail.com>
2022-06-04 21:58:36 +0800

    Force Storing password and custom warning sign for each input

--- ProjectFiles/static/script.js
+++ ProjectFiles/static/script.js
@@ -1,3 +1,9 @@
+document.getElementById("password").addEventListener("invalid", myFunction);
+function myFunction() {
+  document.getElementById("invalid_pass").style.display = 'block';
+}
+
+
 function confirm_password(password, confirm_password){
     if(password!= confirm_password){
         window.alert("password doesn't match!");
